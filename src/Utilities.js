44d6da037e5387630826226a37eba558d24d7ae6
Swanand Mathekar <swanandmathekar@gmail.com>
2023-10-13 18:15:15 +0530

    Add cleanStreamData and renameKey to Utilities

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,14 +1,32 @@
-import { API_ENDPOINTS } from "./API/endpoints";
-
 class Utilities {
-    static async getMovieDetails(id) {
-        try {
-            let res = await fetch(`${API_ENDPOINTS.CONSUMET_URL}movies/flixhq/info?id=${id}`);
-            res = await res.json()
-            return res;
-        } catch (e) {
-            console.log(e);
+    static renameKey(object, oldKey, newKey) {
+        if (oldKey !== newKey) {
+            Object.defineProperty(
+                object,
+                newKey,
+                Object.getOwnPropertyDescriptor(object, oldKey)
+            );
+            delete object[oldKey];
         }
+    }
+
+    static cleanStreamData(streamData) {
+        streamData.sources.map(function (item) {
+            delete item.isM3U8;
+            return item;
+        });
+
+        streamData.sources.forEach((obj) => {
+            this.renameKey(obj, "url", "file");
+            this.renameKey(obj, "quality", "label");
+        });
+
+        streamData.subtitles.forEach((obj) => {
+            this.renameKey(obj, "url", "file");
+            this.renameKey(obj, "lang", "language");
+            obj["lang"] = obj["language"];
+        });
+        return streamData;
     }
 }
 
